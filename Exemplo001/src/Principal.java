7611df253a2eb1892894612851acf5f390efc771
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-02-02 20:12:16 -0300

    Update 02.02.2024

--- Exemplo001/src/Principal.java
+++ Exemplo001/src/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(9 + 2);
+		System.out.println(9 - 2);
+		System.out.println(9 * 2);
+		System.out.println(9 / 2);
 	}
 
 }
---
+++ Exemplo003/src/Principal.java
@@ -0,0 +1,20 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		int valor = 15;
+		
+		if(valor == 8) {
+			System.out.println("Valor = 8");
+			System.out.println("Gostou?");
+		} else if (valor>8) {
+			System.out.println("Valor > 8");
+		} else {
+			System.out.println("Valor < 8");
+		}
+		
+
+	}
+
+}
